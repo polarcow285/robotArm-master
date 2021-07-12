77ff7877da051effd3e8b2ea07493dcbf0102b9c
Natalie Leung <mmoopal@gmail.com>
2021-07-12 16:45:01 -0700

    calculated average speed, calculated real-time speed (speedometer-like) of motor. - had to convert encoder counts per second to revolutions per minute - sampling speed of 1 ms was too short, had to increase to get more accurate rpm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotArmAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotArmAuto.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 //'tag' that displays the opMode on the phone
 public class robotArmAuto extends LinearOpMode{
     public ProjectArm robot = new ProjectArm();
-
+    public static final float ENCODERCOUNTSPERREVOLUTION = 1120f;
     @Override
     public void runOpMode() throws InterruptedException {
         //Initialize with hardwareMap configuration
@@ -21,20 +21,54 @@ public class robotArmAuto extends LinearOpMode{
 
 
         while(opModeIsActive()) {
-            robot.armMotor.setPower(0.6f);
-            telemetry.addData("currentPosition", robot.armMotor.getCurrentPosition());
-            telemetry.update();
-            //sleep(1000);
-            if(robot.armMotor.getCurrentPosition() >= 500){
-                robot.armMotor.setPower(0);
-                break;
+            robot.armMotor.setPower(0.2f);
+            while(robot.armMotor.getCurrentPosition() < 10000){
+                calculateSpeed();
+                //telemetry.addData("currentPosition", robot.armMotor.getCurrentPosition());
+                //telemetry.update();
             }
+            break;
+            /* calculating average speed
+            long startTime = System.currentTimeMillis();
+            robot.armMotor.setPower(0.2f);
+            while(robot.armMotor.getCurrentPosition() < 1120){
+                telemetry.addData("currentPosition", robot.armMotor.getCurrentPosition());
+                telemetry.update();
+            }
+            //sleep(1000);
 
-
+            long endTime = System.currentTimeMillis();
+            robot.armMotor.setPower(0);
+            //calculate
+            long duration = endTime - startTime;
+            telemetry.addData("milliseconds", duration);
+            telemetry.update();
+            //encoder counts per second
+            float distance = 1120;
+            float eps = distance/((float)duration / 1000f);
+            telemetry.addData("eps", eps);
+            telemetry.update();
+            float rpm = eps * (60f/ENCODERCOUNTSPERREVOLUTION);
+            telemetry.addData("rpm", rpm);
+            telemetry.update();
+            sleep(100000);
+            break;
+        */
 
 
         }
         robot.armMotor.setPower(0);
+    }
+    private void calculateSpeed(){
+        float initialPosition = robot.armMotor.getCurrentPosition();
+        sleep(25);
+        float finalPosition = robot.armMotor.getCurrentPosition();
+        float distance = finalPosition-initialPosition;
+        float sampleTimeMS = 25f;
+        float eps = distance/(sampleTimeMS/1000);
+        float rpm = eps * (60f/ENCODERCOUNTSPERREVOLUTION);
+        telemetry.addData("rpm", rpm);
+        telemetry.update();
     }
     private void WaitTillTargetReached(int tolerance){
         int difference = Math.abs(robot.armMotor.getTargetPosition() - robot.armMotor.getCurrentPosition());
